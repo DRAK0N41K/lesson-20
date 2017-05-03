bc9a69db3f71365450fa535f3082bb10f7576c10
DRAK0N41K <d.andriychak@gmail.com>
2017-05-03 21:31:28 +0300

    'final'

---
+++ h20/main.js
@@ -0,0 +1,19 @@
+var mynameis = prompt('input your name');
+console.log('Your name is', mynameis);
+var x = Number(prompt('enter x'));
+var y = Number(prompt('enter y'));
+var z = Number(prompt('enter z'));
+var formyle = x + (y*z);
+prompt(formyle);
+var firstnum = Number(prompt('enter first num.'));
+var secondnum = Number(prompt('enter second num.'));
+var resadd = firstnum - secondnum;
+var ressub = firstnum + secondnum;
+var resmult = firstnum * secondnum;
+var resdiv = firstnum / secondnum;
+var resmov = firstnum % secondnum;
+console.log('add',resadd)
+console.log('sub',ressub)
+console.log('mult',resmult)
+console.log('div',resdiv)
+console.log('mov',resmov)
